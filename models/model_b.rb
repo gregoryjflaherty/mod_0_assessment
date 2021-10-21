b467b4831617614f8b903f935a62b1c3a6b6e454
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-10-21 15:55:36 -0400

    Complete modeling using hashes

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+my_vehicle = {
+  make: "Mercedes",
+  model: "A220",
+  miles: 2100.9,
+  seats: 5,
+  is_owned: false,
+  is_leased: true,
+  int_damage: ['steering wheel', 'passenger seat', 'trunk'],
+  ext_damage: ['left headlight', 'front bumper', 'sunroof']
+}
