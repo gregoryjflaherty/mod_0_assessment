0a54b40c37c20ac330df03f9d1f01e2167963132
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-10-21 15:49:41 -0400

    Complete modeling using hashes

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+airport_lax = {
+  location: "Los Anegeles",
+  state_abbrev: "CA",
+  airline_num: 3,
+  crew_num: 21,
+  is_intl: true,
+  is_rain: false,
+  airlines: ['Delta', 'Spirit', 'Jetblue'],
+  captains: ['Sandra', 'Bob', 'Kelly']
+}
