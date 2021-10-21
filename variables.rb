63472d0e063ec642f2b7c49edd00137edbd9f3b8
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-10-21 15:42:27 -0400

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,16 @@
+famous_person = "Kobe Bryant"
+
+is_active = true
+
+famous_person = "Denzel Washington"
+
+num_3 = 84
+num_9 = 19
+
+sum = num_3 + num_9
+
+animals = ["Dog", "Penguin", "Dolphin"]
+
+animals.delete_at(0)
+
+puts famous_person.length
